9a94fa1fae6307fdbd5aa3ac30e26d4f0e32c853
tafita <andriamboavonjy.tafita@gmail.com>
2025-11-26 13:06:25 +0300

    test : server Go

--- cmd/surgirisk/main.go
+++ cmd/surgirisk/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-    "os"
-
-    "github.com/therecipe/qt/widgets"
-    "surgirisk/internal/ui"
+    "log"
+    "net/http"
 )
 
 func main() {
-   
+    log.Println("Serveur lancé sur http://localhost:8080")
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Serveur Go"))
+    })
+
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
